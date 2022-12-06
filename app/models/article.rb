76c3887754ae27b56936762fd685a40042b11a2c
danmainah <danielmainawaweru@gmail.com>
2022-12-06 23:44:25 +0300

    add search controller and ensure search works

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,20 @@
-class Article < ApplicationRecord
+class Article < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title', 'text']
+          }
+        }
+      }
+    )
+  end
+  mappings do
+    indexes :title,  analyzer: 'english'
+    indexes :text,  analyzer: 'english'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
   resources :articles
+  get 'search', to: 'search#search'
 end
