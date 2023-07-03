a4118d8b3e0df51ecd8cc62989e7d7a6d7d6f872
Danmainah <danielmainawaweru@gmail.com>
2023-07-03 11:49:17 +0300

    add article model and sample articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.create(title: "first article", body: "this is the first article")
+Article.create(title: "second article", body: "this is the second article")
+Article.create(title: "third article", body: "this is the third article")
+Article.create(title: "fourth article", body: "this is the fourth article")
+Article.create(title: "hello article", body: "this is the hello article")
